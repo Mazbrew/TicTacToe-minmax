a325374ff0fd93e17298e3d55236c145c3a30aad
mazbrew <peteryeoh50@gmail.com>
2022-01-22 17:42:54 +0800

    cleanup

--- tictactoeapp/src/App.java
+++ tictactoeapp/src/App.java
@@ -1,7 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        
         Board board = new Board();
         Panel panel = new Panel(board);
         new Frame(panel);
